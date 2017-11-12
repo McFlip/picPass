605025babddbb23ce47e330089c41a337ec28710
Michael Woodham <maw13r@my.fsu.edu>
2017-11-12 16:28:16 -0500

    Entropy only returns true or false

--- entropyTestModule.py
+++ entropyTestModule.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import glob, os
 
-def EntropyMath(text):
+def Entropy(text):
     import math
     log2=lambda x:math.log(x)/math.log(2)
     exr={}
@@ -17,10 +17,30 @@ def EntropyMath(text):
         infoc+=freq*log2(freq)
     infoc*=-1
     return infoc
-    "https://rosettacode.org/wiki/Entropy"
-def Entropy(file1):
-    
-    im = Image.open(file1)
+
+
+file1 = open('PerfectRadom.png','r')
+im = Image.open(file1)
+pix = im.load()
+arrR=[]
+arrG=[]
+arrB=[]
+for x in range(im.size[0]):
+    for y in range(im.size[1]):
+	arrR.append(pix[x,y][0])
+	arrG.append(pix[x,y][1])
+	arrB.append(pix[x,y][2])
+print "Perfectly Random Image"
+print(Entropy(arrR))
+print(Entropy(arrB))
+print(Entropy(arrG))
+
+
+os.chdir("/usr/share/backgrounds")
+
+counter = 0
+for file in glob.glob("*.jpg"):
+    im = Image.open(file)
     pix = im.load()
     arrR=[]
     arrG=[]
@@ -30,4 +50,8 @@ def Entropy(file1):
             arrR.append(pix[x,y][0])
             arrG.append(pix[x,y][1])
             arrB.append(pix[x,y][2])
-    return ((EntropyMath(arrR)) + (EntropyMath(arrB)) + (EntropyMath(arrG)))/3 > 7.1
+    print "image" , counter
+    print(Entropy(arrR))
+    print(Entropy(arrB))
+    print(Entropy(arrG))
+    counter+=1
