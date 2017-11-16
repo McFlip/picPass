0faa36ed6a055d7b1908e5d435007f3b39bbc283
McFlip <gradydenton@yahoo.com>
2017-11-16 16:36:59 -0500

    found bug in b64 encoding

--- prototype/images.js
+++ prototype/images.js
@@ -4,20 +4,67 @@ function download(){
   .replace("image/png", "image/octet-stream");
   download.setAttribute("href", image);
 }
-// function hexToBase64(str) {
-//   return btoa(String.fromCharCode.apply(null, str.replace(/\r|\n/g, "").replace(/([\da-fA-F]{2}) ?/g, "0x$1 ").replace(/ +$/, "").split(" ")));
-// }
+
 function dataUrlToBase64 (dataURL){
   return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
 }
-function stego(buffer) { // buffer is an ArrayBuffer
-  // hex will be fed into the word encoding function
-//   var hex = Array.prototype.map.call(new Uint8Array(buffer), x => ('00' + x.toString(16)).slice(-2)).join('');
-  var rando = 1;  //TODO: Use a secure random function to flip between 0 & 1 each time it's used
+
+// function base64toHEX(base64) {
+//   var raw = atob(base64);
+//   var HEX = '';
+//   for ( i = 0; i < raw.length; i++ ) {
+//     var _hex = raw.charCodeAt(i).toString(16);
+//     HEX += (_hex.length==2?_hex:'0'+_hex);
+//   }
+//   return HEX.toUpperCase();
+// }
+var keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/="
+function base64_decode(input) {
+  var output = new Array();
+  var chr1, chr2, chr3;
+  var enc1, enc2, enc3, enc4;
+  var i = 0;
+
+  var orig_input = input;
+  input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+  if (orig_input != input)
+    alert ("Warning! Characters outside Base64 range in input string ignored.");
+  if (input.length % 4) {
+    alert ("Error: Input length is not a multiple of 4 bytes.");
+    return "";
+  }
+
+  var j=0;
+  while (i < input.length) {
+
+    enc1 = keyStr.indexOf(input.charAt(i++));
+    enc2 = keyStr.indexOf(input.charAt(i++));
+    enc3 = keyStr.indexOf(input.charAt(i++));
+    enc4 = keyStr.indexOf(input.charAt(i++));
+
+    chr1 = (enc1 << 2) | (enc2 >> 4);
+    chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+    chr3 = ((enc3 & 3) << 6) | enc4;
+
+    output[j++] = chr1;
+    if (enc3 != 64)
+      output[j++] = chr2;
+    if (enc4 != 64)
+      output[j++] = chr3;
+
+  }
+  return output;
+}
+function stego(buffer) { // buffer is dataURL
+  // randomisation for STEGO
+  var rando = 1;  // Use a secure random function to flip between 0 & 1 each time it's used
+  var b64 = dataUrlToBase64(buffer);
+  var words = CryptoJS.enc.Base64.parse(b64);
+  var key = new Date().getTime();  // timestamp in milliseconds
+  var cipherText = CryptoJS.RC4Drop.encrypt(words, "Secret Passphrase");
   var canvas = document.getElementById("canvas");
   var ctx=canvas.getContext("2d");
   var image = new Image();
-//   image.src = 'data:image/jpeg;base64,' + hexToBase64(hex);
   image.src = buffer;
   fileDisplayArea.innerHTML = "";
   fileDisplayArea.appendChild(image);
@@ -29,10 +76,14 @@ function stego(buffer) { // buffer is an ArrayBuffer
     var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
     var data = imageData.data;
     // STEGO
+
     for(var i = 0, n = data.length; i < n; i += 4) {
-      data[i] ^= rando;
+
+//       data[i] ^= rando;
+      data[i] = 0;
       data[i + 1] ^= rando;
-      data[i + 2] ^= rando;
+//       data[i + 2] ^= rando;
+      data[i + 2] = 255;
       data[i + 3] ^= rando;
     }
     ctx.putImageData(imageData, 0, 0);
@@ -40,6 +91,7 @@ function stego(buffer) { // buffer is an ArrayBuffer
 
   return dataUrlToBase64(canvas.toDataURL("image/png"));
 }
+
 window.onload = function() {
 
   var fileInput = document.getElementById('fileInput');
@@ -57,8 +109,13 @@ window.onload = function() {
       reader.onload = function(event) {
         var binary = event.target.result;
         var b64 = stego(binary);
+        var original = dataUrlToBase64(binary);
+        var ohex = base64_decode(binary);
+        var owords = CryptoJS.enc.Hex.parse(ohex);
         var words = CryptoJS.enc.Base64.parse(b64);
         var sha256 = CryptoJS.SHA256(words);
+        var osha = CryptoJS.SHA256(owords);
+        alert(osha.toString());
 
         document.getElementById('sha').value = sha256.toString();
         document.getElementById('shab64').value = sha256.toString(CryptoJS.enc.Base64);
